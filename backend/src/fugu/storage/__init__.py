32312fe328008e23fbc804a17afcafba5cdfee6e
Shravan Prassadh <99738289+shravanprassadh@users.noreply.github.com>
2026-07-10 19:25:16 +0530

    Create storage package

---
+++ backend/src/fugu/storage/__init__.py
@@ -0,0 +1,5 @@
+"""Object-storage abstractions for Fugu."""
+
+from fugu.storage.objects import ObjectStorage, PresignedOperation, StoredObjectMetadata
+
+__all__ = ["ObjectStorage", "PresignedOperation", "StoredObjectMetadata"]
